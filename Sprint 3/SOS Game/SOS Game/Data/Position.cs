862070d4785f3ee3d11207144006c1944e21490c
csk2q <143836825+csk2q@users.noreply.github.com>
2024-10-21 12:57:23 -0500

    Rename Position to Records - added Sos record

--- Sprint 3/SOS Game/SOS Game/Data/Position.cs
+++
@@ -1,3 +0,0 @@
-namespace SOS_Game;
-
-public record Position(int row, int column);
---
+++ Sprint 3/SOS Game/SOS Game/Data/Records.cs
@@ -0,0 +1,5 @@
+namespace SOS_Game;
+
+public record Position(int row, int column);
+
+public record Sos(Position S1, Position O, Position S2);
