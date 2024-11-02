cfe57bcf623416310f41fbbe5dc146b1ae020ae2
csk2q <143836825+csk2q@users.noreply.github.com>
2024-11-02 15:57:22 -0500

    Continue work on computer player moves

--- Sprint 4/SOS Game/SOS Game/Data/Records.cs
+++ Sprint 4/SOS Game/SOS Game/Data/Records.cs
@@ -3,3 +3,7 @@ namespace SOS_Game;
 public record Position(int row, int column);
 
 public record Sos(Position S1, Position O, Position S2);
+
+public record Move(TileType Tile, Position Position);
+
+public record TurnResult(Move Move, Sos[] SosMade);
--- Sprint 4/SOS Game/SOS Game/Logic/GameBoard.cs
+++ Sprint 4/SOS Game/SOS Game/Logic/GameBoard.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace SOS_Game.Logic;
 
@@ -20,15 +21,16 @@ public abstract class GameBoard
     public PlayerType PlayerTypeTurn { get; private set; } = PlayerType.BlueLeft;
     public Player Blue { get; init; }
     public Player Red { get; init; }
-    
+
     // Private Variables //
 
-    protected readonly int size;
-    
-
-    // (Row, Column) 0,0 is top left.
+    //(Row, Column) 0,0 is top left.
     private TileType[][] board;
+    private readonly int size;
 
+    Random random = new Random();
+
+    //Turn stuff
     protected record Turn(PlayerType Player, Position Position, TileType TileType);
 
     protected List<Turn> turnRecord = [];
@@ -45,7 +47,7 @@ public abstract class GameBoard
         for (int i = 0; i < size; i++)
             board[i] = new TileType[size];
     }
-    
+
     // Create Method //
 
     public static GameBoard CreateNewGame(GameType gameType, int boardSize, bool isBlueComputer, bool isRedComputer)
@@ -96,9 +98,25 @@ public abstract class GameBoard
         else
             return PlayerType.None;
     }
-    
-    // TODO add GetBoardState()
-    // It will return a yeld-able result? 
+
+    public bool IsCurentPlayerComputer()
+    {
+        if (PlayerTypeTurn == PlayerType.BlueLeft)
+            return Blue.IsComputer;
+        if (PlayerTypeTurn == PlayerType.RedRight)
+            return Red.IsComputer;
+        return false;
+    }
+
+    /*
+     * TODO implement GetBoardState()
+     * Should it call
+     * It will return a yeld-able result?
+     */
+    public TileType[][] GetBoardState()
+    {
+        return board;
+    }
 
     // Business Functions //
 
@@ -110,7 +128,7 @@ public abstract class GameBoard
             completedSosArray = [];
             return false;
         }
-        
+
         if (tileType is not TileType.None // If the placing tile is S or O
             && row < size && column < size // AND position is valid 
             && board[row][column] == TileType.None) // AND Clicked tile is empty
@@ -163,7 +181,7 @@ public abstract class GameBoard
         return false;
     }
 
-    Sos[] checkSos(int row, int column)
+    private Sos[] checkSos(int row, int column)
     {
         var placedTile = board[row][column];
         List<Sos> newSoSes = [];
@@ -289,5 +307,78 @@ public abstract class GameBoard
         }
 
         return newSoSes.ToArray();
+    }
+
+    // Call to have computer players make their moves?
+    private List<TurnResult> tickComputerPlayers()
+    {
+        List<TurnResult> moves = [];
+
+        while (IsCurentPlayerComputer() && !IsGameOver())
+        {
+            var newMove = makeComputerMove();
+            moves.Add(newMove);
+        }
+
+        return moves;
+    }
+
+    private TurnResult makeComputerMove()
+    {
+        // TODO read AC 8 for the logic to implement
+        /*
+         * AC 8.1 Computer takes the first turn
+         * Given: A new game is started
+         * And: The first turn is the computer’s
+         * When: The first turn of the game is the computer’s
+         * Then: Place a tile of either type at random on the board.
+         */
+        if (turnRecord.Count == 0)
+        {
+            bool success;
+            Sos[] completedSosArray;
+            int row;
+            int column;
+            TileType tileType;
+
+            do
+            {
+                // Randomly choose location
+                row = random.Next(0, size);
+                column = random.Next(0, size);
+
+                // Randomly choose the tile either S(1) or O(2)
+                tileType = (TileType)random.Next(1, 3);
+
+                success = PlaceTile(row, column, tileType, out completedSosArray);
+
+                Debug.Assert(success, "Failed to place tile?");
+            } while (!success);
+
+            return new TurnResult(new Move(tileType, new Position(row, column)), completedSosArray);
+        }
+
+        /*
+         * AC 8.2 Computer makes an SOS
+         * Given: An ongoing game
+         * When: It is the computer’s turn
+         * And: A valid SOS can be completed
+         * Then: The computer should place the tile needed to complete the SOS
+         */
+
+        /*
+         * AC 8.3 Computer makes a blocking move
+         * Given: An ongoing game
+         * When: It is the computer’s turn
+         * And: There are no SOSes to complete
+         * Then: The computer should attempt to make a “blocking” move so that the human cannot score a point next turn if possible.
+         * */
+
+        /*
+         * AC 8.4 Computer makes a random Move
+         * Given: And ongoing game
+         * When: The computer cannot make an SOS nor make a blocking move.
+         * Then: The computer will make a random valid move.
+         */
     }
 }
